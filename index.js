92c7046059277836d96e76529fcaa8b867b029a1
Lucas Stettner <lucas.stettner@gmail.com>
2019-01-25 19:55:42 -0500

    fixed index.js

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ const fields = ['title', 'user', 'date'];
 const opts = { fields };
 const parser = new Json2csvParser(opts);
 
-let pull = []
+let pullTitles = []
 
 octokit.authenticate({ // Auth in order to get more calls
     type: 'token',
     token: token
-  })
+})
 
 octokit.paginate('GET /repos/:owner/:repo/pulls?state=closed', { owner: 'LucasStettner', repo: 'LucasTip' })
-  .then(pulls => { // returns an array
+    .then(pulls => { // returns an array
 
-      pulls.forEach(pull => {
+        pulls.forEach(pull => {
 
-          if(pull.head.ref == 'master' && pull.base.ref == 'prod' && pull.merged_at != null){ // checks if coming from master and going to prod and not closed
-            let date = moment(pull.merged_at).format("MM/DD/YYYY");
+            if (pull.head.ref == 'master' && pull.base.ref == 'prod' && pull.merged_at != null) { // checks if coming from master and going to prod and not closed
+                let date = moment(pull.merged_at).format("MM/DD/YYYY");
 
-            repoNames.push({title:pull.title,user:pull.user.login,date:pull.merged_at.slice(0, -10)})
-          }
-      });
+                pullTitles.push({ title: pull.title, user: pull.user.login, date: pull.merged_at.slice(0, -10) })
+            }
+        });
 
-    // parse repoNames as csv
-    const csv = parser.parse(repoNames);
+        // parse pullTitles as csv
+        const csv = parser.parse(pullTitles);
 
-    fs.writeFile('closedPull.csv', csv, 'utf8', function (err) { // write csv to file
-        if (err) {
-            console.log(err);
-        } else{
-            console.log("saved!")
-        }
+        fs.writeFile('closedPull.csv', csv, 'utf8', function (err) { // write csv to file
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("saved!")
+            }
+        });
+
     });
-
-});
